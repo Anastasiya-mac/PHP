585fdde926831363a09959926d6d1de32e850e69
Anastasiya-mac <sunshine_0803@mail.ru>
2020-05-22 15:40:29 +0300

    delete

--- lesson_6/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    "host" => "127.0.0.1",
-    "user_name" => "root",
-    "user_password" => "",
-    "db_name" => "GB_DB_PHP"
-]
-?>
--- lesson_6/views/catalog.php
+++
@@ -1,8 +0,0 @@
-<h1>Каталог товаров</h1>
-<?php foreach ($product as $item): ?>
-    <div class="product">
-        <a href="http://192.168.64.2/myFolder/public/product.php?id=<?=$item['id_product']?>"> 
-            <h2><?=$item['name_product']?></h2>
-        </a>
-    </div>
-<?php endforeach; ?>
